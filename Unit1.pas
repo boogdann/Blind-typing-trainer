cdac45a423602f2b4af1412567af07e0b445c8e5
bogdan <griduskobogdan@gmail.com>
2022-12-14 19:24:29 +0300

    add doubling letters, with an error

--- Unit1.pas
+++ Unit1.pas
@@ -11,6 +11,7 @@ type
     memoStringOutput: TMemo;
     edtStringFromUser: TEdit;
     procedure edtStringFromUserKeyPress(Sender: TObject; var Key: Char);
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -21,14 +22,16 @@ var
   fmMainLogic: TfmMainLogic;
 Var
   UserString: string;
-  RandomString: String;
+  RandomString: String = 'qwertyu asdf rty';
   MaxLength: Integer = 20;
   TempForDuplicate: Integer = 2;
   PosUserStr, PosRandomStr, j, k: Integer;
   UserPart, RandomPart: string;
   OutputString: string;
   MinLength: Integer;
-
+  MainLengthUserStr: Integer;
+  kolElementsUserStr: Integer = 0;
+  kolElementsRandomStr: INteger = 0;
 implementation
 
 {$R *.dfm}
@@ -37,36 +40,62 @@ procedure TfmMainLogic.edtStringFromUserKeyPress(Sender: TObject; var Key: Char)
 var
   i: Integer;
 begin
-  if not (Key in ['a'..'z', 'A'..'Z' ,#13, '1', '3']) then
+  if not (Key in ['a'..'z', 'A'..'Z' ,#13, '1', '3', ' ']) then
     Key := #0;
+
+  UserString := edtStringFromUser.Text;
+  MainLengthUserStr := Length(UserString);
+  SetLength(UserString, Length(RandomString));
+  if (UserString = '13') then
+  begin
+    ShowMessage('Игра окончена');
+    edtStringFromUser.Clear;
+    memoStringOutput.Clear;
+  end;
 
   if (Key = #13) then
   begin
-    ShowMessage('WORKK!!!');
+    // ShowMessage('WORKK!!!');
     if (UserString = RandomString) then
     begin
-      // random new string
+      // рандом новго слова
+      MaxLength := MaxLength - 2;
+      //
+      //
+      //
     end;
 
-    if (Length(UserString) > Length(RandomString)) then
-      MinLength := Length(RandomString)
+    if (Length(UserString) <= Length(RandomString)) then
+      MaxLength := Length(RandomString)
     else
-      MinLength := Length(UserString);
+      MaxLength := Length(UserString);
 
     PosUserStr := 0;
     PosRandomStr := 0;
 
     i := 1;
-    while (i <= Length(UserString)) do
+    while (i <= Length(RandomString)) do
     begin
-      while (UserString[PosUserStr] <> ' ') do
+      kolElementsUserStr := 0;
+      kolElementsRandomStr := 0;
+
+      while ((UserString[PosUserStr] <> ' ') and (PosUserStr <= MainLengthUserStr)) do
+      begin
         Inc(PosUserStr);
+        Inc(kolElementsUserStr);
+      end;
 
-      while (RandomString[PosRandomStr] <> ' ') do
+      // if (i <> 1) then
+        // Inc(PosRandomStr);
+
+      while ((RandomString[PosRandomStr] <> ' ') and (PosRandomStr <= Length(RandomString))) do
+      begin
         Inc(PosRandomStr);
+        INc(kolElementsRandomStr);
+      end;
 
-      UserPart := Copy(UserString, i, PosUserStr);
-      RandomPart := Copy(RandomString, i, PosRandomStr);
+      UserPart := Copy(UserString, PosUserStr - kolElementsUserStr, kolElementsUserStr);
+      RandomPart := Copy(RandomString, i, kolElementsRandomStr);
 
       if (UserPart = RandomPart) then
       begin
@@ -76,89 +105,51 @@ begin
       else
       begin
 
-        if (Length(UserPart) >= Length(RandomPart)) then
+        j := 1;
+        while ((j <= Length(RandomPart)) and (UserPart[i] <> ' ')) do
         begin
-          j := 0;
-          while (j <= Length(RandomPart)) do
+          if (UserPart[j] <> RandomPart[j]) then
           begin
-            if (UserPart[j] <> RandomPart[j]) then
-            begin
-              for k := 1 to TempForDuplicate div 2 do
-                OutputString := OutputString + UserPart[j];
-            end;
-            Inc(j);
-          end;
-
-          j := Length(RandomPart) + 1;
-          while (j < Length(UserPart)) do
             for k := 1 to TempForDuplicate div 2 do
-              OutputString := OutputString + UserPart[j];
-
-        end
-        else
-        begin
-
-          j := 0;
-          while (j <= Length(UserPart)) do
-          begin
-            if (UserPart[j] <> RandomPart[j]) then
-            begin
-              for k := 1 to TempForDuplicate div 2 do
-                OutputString := OutputString + UserPart[j];
-            end;
-            Inc(j);
+              OutputString := OutputString + RandomPart[j];
           end;
-
-          j := Length(RandomPart) + 1;
-          while (j < Length(UserPart)) do
-            for k := 1 to TempForDuplicate div 2 do
-              OutputString := OutputString + UserPart[j];
-        end
-
+            OutputString := OutputString + RandomPart[j];
+          Inc(j);
+        end;
       end;
 
-      i := i + PosUserStr;
-    end;
+      Inc(PosRandomStr);
+      Inc(PosUserStr);
+      // PosUserStr := PosRandomStr;
+      i := PosRandomStr;
 
+    end;
+    memoStringOutput.Clear;
+    memoStringOutput.Lines[0] := OutputString;
+  end;
+   memoStringOutput.Clear;
+
+
+  if (Length(UserString) >= 60) then
+  begin
+    ShowMessage('Игра проиграна. Попробуйте ещё');
+    // начало игры заново !!!!!!!!!!!!!!!!!!!!!!!!!!!1
+  end;
+  Inc(i);
+
+  if (MaxLength = 0) then
+  begin
+    TempForDuplicate := TempForDuplicate * 2;
+    // рандом новой строки
   end;
 
-    memoStringOutput.Lines[0] := OutputString;
+end;
 
-    if (Length(UserString) >= 60) then
-    begin
-      ShowMessage('Игра проиграна. Попробуйте ещё');
-      // начало игры заново !!!!!!!!!!!!!!!!!!!!!!!!!!!1
-    end;
-    Inc(i);
-
-    memoStringOutput.Lines[0] := UserString;
-
-
-  UserString := edtStringFromUser.Text;
+procedure TfmMainLogic.FormCreate(Sender: TObject);
+begin
+  memoStringOutput.Lines[0] := RandomString;
 end;
 
 end.
 
-//procedure TfmMainLogic.memoStringOutputEnter(Sender: TObject);
-//var
-//  i: Integer;
-//begin
-//  ShowMessage('Игра проиграна. Попробуйте ещё');
-//  i := 1;
-//  while (i < Length(UserString)) do
-//  begin
-//    if (UserString[i] <> RandomString[i]) then
-//      for j := 1 to Temp4Duplicate div 2 do
-//      begin
-//        Insert(UserString[i], RandomString, i);
-//      end;
-//    Inc(i);
-//  end;
-//
-//  if (Length(UserString) >= 60) then
-//  begin
-//    ShowMessage('Игра проиграна. Попробуйте ещё');
-//    // начало игры заново !!!!!!!!!!!!!!!!!!!!!!!!!!!1
-//  end;
-//  memoStringOutput.Lines[0] := UserString;
-//end;
+
